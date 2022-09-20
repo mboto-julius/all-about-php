90f80fffb6e308aa02d0965af066f3f4ea4dda54
Julius Mboto <juliusmboto@gmail.com>
2022-09-20 11:44:53 +0300

    this keyword inside class

--- php/oop.php
+++ php/oop.php
@@ -1,44 +1,17 @@
 <?php
 
+// $this keyword refers to the current object and is only available inside a methods
+
 class Fruit
 {
-    //properties
     public $name;
-    public $color;
-
-    // methods
     function set_name($name)
     {
         $this->name = $name;
     }
-
-    function get_name()
-    {
-        return $this->name;
-    }
-
-    function set_color($color)
-    {
-        $this->color = $color;
-    }
-
-    function get_color()
-    {
-        return $this->color;
-    }
 }
 
 $apple = new Fruit();
-$banana = new Fruit();
-$apple->set_name('apple');
-$apple->set_color('green');
-$banana->set_name('banana');
-$banana->set_color('yellows');
+$apple->set_name('Apple');
 
-echo $apple->get_name();
-echo '<br>';
-echo $apple->get_color();
-echo '<br>';
-echo $banana->get_name();
-echo '<br>';
-echo $banana->get_color();
+echo $apple->apple;

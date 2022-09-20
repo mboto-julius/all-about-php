3a8b967c15455174284b76bf05716e6029585e6b
Julius Mboto <juliusmboto@gmail.com>
2022-09-20 14:17:59 +0300

    php inheritence

--- php/oop.php
+++ php/oop.php
@@ -1,23 +1,30 @@
 <?php
 
-// A constructor allows you to initialize an object's properties upon creation of the object.
-
 class Fruit
 {
-    public $name;
-    public $color;
+   public $name;
+   public $color;
+
+   public function __construct($name, $color){
+    $this->name = $name;
+    $this->color = $color;
+   }
+
+   public function intro(){
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+   }
+}
 
 
-    function __construct($name, $color)
-    {
-        $this->name = $name;
-        $this->color = $color;
-    }
-
-    function __destruct()
-    {
-        echo "The fruit is {$this->name} and the color is {$this->color}.";
+// Strawberry is inherited from Fruit
+class Strawberry extends Fruit{
+    public function message(){
+        echo "Am I a fruit or a berry?";
     }
 }
 
-$apple = new Fruit("apple", "green");
+
+$strawberry = new Strawberry("Strawberry", "red");
+$strawberry->message();
+echo "<br>";
+$strawberry->intro();

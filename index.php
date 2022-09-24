aa70b1e388e3636eefbd2b5113765f441f8c3c97
Julius Mboto <juliusmboto@gmail.com>
2022-09-24 20:11:29 +0300

    defining properties

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 
 class Cars{
 
-  function greating(){
+  var $wheel_count = 4;
+  var $door_count = 10;
 
-    echo "Hello Student";
+  function carDetails(){
+
+    return "This car has " .$this->wheel_count. " Wheels"; 
 
   }
 
@@ -13,8 +16,18 @@ class Cars{
 
 // instantiating a class
 $bmw = new Cars();
-
 $mercedes = new Cars();
 
 // output from function
-$bmw->greating();
+echo "<br>";
+echo $bmw->wheel_count;
+echo "<br>";
+echo $bmw->door_count;
+echo "<br>";
+echo $bmw->carDetails();
+echo "<br>";
+echo $mercedes->carDetails();
+echo "<br>";
+echo $mercedes->door_count;
+echo "<br>";
+echo $mercedes->wheel_count;

876bc6312089ff227774b10deac6a570b7b8f5ef
Julius Mboto <juliusmboto@gmail.com>
2022-09-24 19:02:50 +0300

    check all methods for class

--- index.php
+++ index.php
@@ -3,12 +3,36 @@
 
 class Cars{
 
+  function greating(){
+
+  }
+
+  function greating1(){
+
+  }
+
+  function greating2(){
+    
+  }
+
+  function greating3(){
+    
+  }
+
 }
 
-$my_classes = get_declared_classes();
+// $my_classes = get_declared_classes();
 
-foreach($my_classes as $class){
-  echo $class . "<br>";
+// foreach($my_classes as $class){
+//   echo $class . "<br>";
+// }
+
+// getting methods of classes
+
+$the_methods = get_class_methods('Cars');
+
+foreach ($the_methods as $method){
+  echo $method . "<br>";
 }
 
 ?>

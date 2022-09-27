8c8d95b14835f5823f7ad19af389faec0c89159b
Julius Mboto <juliusmboto@gmail.com>
2022-09-27 18:44:53 +0300

    add includes files

--- index.php
+++ index.php
@@ -6,18 +6,7 @@ require 'dbconnection.php';
 
 ?>
 
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Student Details</title>
-  </head>
-  <body>
+<?php include('includes/header.php'); ?>
 
     <div class="container mt-5">
       <!-- message appear  -->
@@ -82,8 +71,4 @@ require 'dbconnection.php';
       </div>
     </div>
 
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <?php include('includes/footer.php'); ?>

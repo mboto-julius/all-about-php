0eb461be7ae92018a17e78371bd606a67c352b4a
Julius Mboto <juliusmboto@gmail.com>
2022-09-27 15:41:59 +0300

    read data from database

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+// include database
+require 'dbconnection.php';
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,7 +17,64 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4>Student details
+              <a href="student-create.php" class="btn btn-primary float-end">Add Students</a>
+              </h4>
+            </div>
+            <div class="card-body">
+              <table class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Course</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php  
+                  // fetch data from database
+                  $query = "SELECT * FROM students";
+                  $query = mysqli_query($connection, $query);
+
+                  // check the number of rows, if is greater than zero => show the number of rows
+                  if(mysqli_num_rows($query) > 0){
+                    foreach($query as $student){
+                  ?>
+                      <tr>
+                        <td><?= $student['id']; ?></td>
+                        <td><?= $student['name']; ?></td>
+                        <td><?= $student['email']; ?></td>
+                        <td><?= $student['phone']; ?></td>
+                        <td><?= $student['course']; ?></td>
+                        <!-- add action button -->
+                        <td>
+                          <a href="" class="btn btn-info btn-sm">View</a>
+                          <a href="" class="btn btn-success btn-sm">Edit</a>
+                          <a href="" class="btn btn-danger btn-sm">Delete</a>
+                        </td>
+                      </tr>
+                  <?php
+                    }
+                  }else{
+                    echo "<h5>No records found</h5>";
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
 
